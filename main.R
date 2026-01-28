3c6f7e75b45b660a49891bb3f92c3fcfa07d9a64
yabibereket21 <yabibereket7@gmail.com>
2026-01-28 09:42:45 -0500

    Update main.R

--- main.R
+++ main.R
@@ -2,8 +2,11 @@ library(tidyverse)
 library(here)
 
 tv_hours_table <- gss_cat %>%
+  filter(age >= 30)%>%
   group_by(marital) %>%
   summarise(mean_tv_hours = mean(tvhours, na.rm = T))
 
 
 write_csv(tv_hours_table, here("TV Hours by Marital.csv"))
+
+
