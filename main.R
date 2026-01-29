70553f0b813af0d5765523a8733434cd28ac9a95
yabibereket21 <yabibereket7@gmail.com>
2026-01-29 11:43:29 -0500

    shorten names

--- main.R
+++ main.R
@@ -1,12 +1,12 @@
 library(tidyverse)
 library(here)
 
-tv_hours_table <- gss_cat %>%
+tv_hours_tab <- gss_cat %>%
   filter(age < 30)%>%
   group_by(marital) %>%
   summarise(mean_tv_hours = mean(tvhours, na.rm = T))
 
 
-write_csv(tv_hours_table, here("TV Hours by Marital.csv"))
+write_csv(tv_hours_tab, here("TV Hours by Marital.csv"))
 
 
